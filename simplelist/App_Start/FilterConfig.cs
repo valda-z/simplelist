dec8ab3f72d8c410779acdceec88c90602692855
valdaz <valdemar.zavadsky@gmail.com>
2016-09-21 13:15:14 +0200

    Add project files.

---
+++ simplelist/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace simplelist
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
