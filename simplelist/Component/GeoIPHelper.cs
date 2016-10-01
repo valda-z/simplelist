e1cd9a1fa54d981f3fbed1bc0751c853e488305e
valdaz <valdemar.zavadsky@gmail.com>
2016-10-01 18:21:29 +0200

    geoip service integrated to project

---
+++ simplelist/Component/GeoIPHelper.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace simplelist.Component
+{
+    public class GeoIPHelper
+    {
+        public class Names
+        {
+            public string en { get; set; }
+            public string ru { get; set; }
+        }
+
+        public class City
+        {
+            public int GeoNameID { get; set; }
+            public Names Names { get; set; }
+        }
+
+        public class Names2
+        {
+            public string fr { get; set; }
+            public string ja { get; set; }
+            public string __invalid_name__pt_BR { get; set; }
+            public string ru { get; set; }
+            public string __invalid_name__zh_CN { get; set; }
+            public string de { get; set; }
+            public string en { get; set; }
+            public string es { get; set; }
+        }
+
+        public class Continent
+        {
+            public string Code { get; set; }
+            public int GeoNameID { get; set; }
+            public Names2 Names { get; set; }
+        }
+
+        public class Names3
+        {
+            public string ru { get; set; }
+            public string __invalid_name__zh_CN { get; set; }
+            public string de { get; set; }
+            public string en { get; set; }
+            public string es { get; set; }
+            public string fr { get; set; }
+            public string ja { get; set; }
+            public string __invalid_name__pt_BR { get; set; }
+        }
+
+        public class Country
+        {
+            public int GeoNameID { get; set; }
+            public string IsoCode { get; set; }
+            public Names3 Names { get; set; }
+        }
+
+        public class Location
+        {
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+            public int MetroCode { get; set; }
+            public string TimeZone { get; set; }
+        }
+
+        public class Postal
+        {
+            public string Code { get; set; }
+        }
+
+        public class Names4
+        {
+            public string ja { get; set; }
+            public string __invalid_name__pt_BR { get; set; }
+            public string ru { get; set; }
+            public string __invalid_name__zh_CN { get; set; }
+            public string de { get; set; }
+            public string en { get; set; }
+            public string es { get; set; }
+            public string fr { get; set; }
+        }
+
+        public class RegisteredCountry
+        {
+            public int GeoNameID { get; set; }
+            public string IsoCode { get; set; }
+            public Names4 Names { get; set; }
+        }
+
+        public class RepresentedCountry
+        {
+            public int GeoNameID { get; set; }
+            public string IsoCode { get; set; }
+            public object Names { get; set; }
+            public string Type { get; set; }
+        }
+
+        public class Names5
+        {
+            public string de { get; set; }
+            public string en { get; set; }
+            public string fr { get; set; }
+        }
+
+        public class Subdivision
+        {
+            public int GeoNameID { get; set; }
+            public string IsoCode { get; set; }
+            public Names5 Names { get; set; }
+        }
+
+        public class Traits
+        {
+            public bool IsAnonymousProxy { get; set; }
+            public bool IsSatelliteProvider { get; set; }
+        }
+
+        public class Data
+        {
+            public City City { get; set; }
+            public Continent Continent { get; set; }
+            public Country Country { get; set; }
+            public Location Location { get; set; }
+            public Postal Postal { get; set; }
+            public RegisteredCountry RegisteredCountry { get; set; }
+            public RepresentedCountry RepresentedCountry { get; set; }
+            public List<Subdivision> Subdivisions { get; set; }
+            public Traits Traits { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Data Data { get; set; }
+        }
+    }
+}
--- simplelist/Controllers/HomeController.cs
+++ simplelist/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using simplelist.Models;
+﻿using RestSharp;
+using simplelist.Component;
+using simplelist.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +11,39 @@ namespace simplelist.Controllers
 {
     public class HomeController : Controller
     {
+        private string getGeoIPData(string ip)
+        {
+            if (ip == "::1")
+            {
+                ip = "46.228.25.202";
+            }
+            var client = new RestClient(Properties.Settings.Default.GEOIPURL);
+            var request = new RestRequest("/" + ip, Method.GET);
+            var queryResult = client.Execute<GeoIPHelper.RootObject>(request).Data;
+            if (queryResult.Data == null)
+            {
+                return string.Format(" Your IP [{0}] - invalid search argument", ip);
+            }
+            else
+            {
+                return string.Format(" Your IP [{0}] - {1}, {2}, {3}/{4}", 
+                    ip,
+                    queryResult.Data.Country.Names.en,
+                    queryResult.Data.City.Names.en,
+                    queryResult.Data.Location.Latitude, 
+                    queryResult.Data.Location.Longitude);
+            }
+        }
+
         public ActionResult Index()
         {
+            ViewBag.LinuxData = "VNET not connected!";
+            try
+            {
+                ViewBag.LinuxData = getGeoIPData(Request.UserHostAddress);
+            }
+            catch { }
+
             return View(new MyModel().MyEntitySamples.OrderBy(e => e.Name));
         }
 
